b0d6ea2699725c18fd88d95d9ca062504e2cff8f
CH4RM80 <61711074+CH4RM80@users.noreply.github.com>
2021-03-03 10:25:06 -0600

    changed functions
    added the getRandomColor function

--- banyesyes.js
+++ banyesyes.js
@@ -1,5 +1,12 @@
 const {Client, MessageEmbed, Message, GuildManager, GuildMember, DiscordAPIError, Discord, ClientUser} = require('discord.js');
-
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
 module.exports = client => {
     client.on('guildBanAdd', async (guild, user) => {
         channel = guild.channels.cache.find(i => i.name === "mod-logs")
--- messagedelete.js
+++ messagedelete.js
@@ -1,5 +1,13 @@
 const {Client, MessageEmbed, Message, GuildManager, GuildMember, DiscordAPIError, Discord, ClientUser} = require('discord.js');
 
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
 module.exports = client => {
     client.on('messageDelete', (messageDelete) => {
         channel = messageDelete.guild.channels.cache.find(i => i.name === "mod-logs")
