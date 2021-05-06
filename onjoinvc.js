a309f2c593bf087dbdef66a83ccfe6e138052e86
CH4RM80 <61711074+CH4RM80@users.noreply.github.com>
2021-05-06 12:45:00 -0500

    Update onjoinvc.js
    added extra code for leaving empty voice channels

--- onjoinvc.js
+++ onjoinvc.js
@@ -1,5 +1,6 @@
 const { OpusEncoder } = require('@discordjs/opus');
 const {Client, MessageEmbed, Message, GuildManager, GuildMember, DiscordAPIError, Discord, ClientUser} = require('discord.js');
+const { readSync } = require('node:fs');
 
 async function play(voiceChannel) {
     const connection = await voiceChannel.join();
@@ -9,7 +10,16 @@ async function play(voiceChannel) {
 module.exports = client => {
     client.on('voiceStateUpdate', (oldState, newState) => {
         if (oldState.member.user.bot) return;
-        else if (newState.channelID === null) return;
+        else if (newState.channelID === null) {
+            const mychannel = client.channels.cache.get("839915352603361320")
+            try {
+                mychannel.leave()
+            }
+            catch (Error) {
+                console.log("The bot was not in a vc")
+                return;
+            }
+        }
         else if (newState.channelID === "839915352603361320") {
             const mychannel = client.channels.cache.get("839915352603361320")
             play(mychannel)
